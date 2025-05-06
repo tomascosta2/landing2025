37ad7f492022612a286ced2dfae813f74b3a36c2
tomas <tomascostapp@gmail.com>
2025-05-06 15:14:30 -0300

    D

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,34 @@
+import { r as renderers } from './chunks/internal_BsTt5pTQ.mjs';
+import { c as createExports } from './chunks/entrypoint_DEldKSKg.mjs';
+import { manifest } from './manifest_BsgVuTsb.mjs';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/track/lead.astro.mjs');
+const _page2 = () => import('./pages/b.astro.mjs');
+const _page3 = () => import('./pages/landingbuilderslab.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/api/track/lead.ts", _page1],
+    ["src/pages/b/index.astro", _page2],
+    ["src/pages/landingbuilderslab/index.astro", _page3],
+    ["src/pages/index.astro", _page4]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "90a59869-88a9-46fe-bbc5-51ec00e47591",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_DgRVF5Gt.mjs';
+export { r as renderers } from '../chunks/internal_BsTt5pTQ.mjs';
