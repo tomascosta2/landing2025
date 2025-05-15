9be55763355cc410603d34c742c3b6798ee27efc
tomas <tomascostapp@gmail.com>
2025-05-15 15:19:09 -0300

    Mobile

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_BXwa9WrT.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_B_bq9DAV.mjs';
-export { r as renderers } from '../chunks/internal_BsTt5pTQ.mjs';
