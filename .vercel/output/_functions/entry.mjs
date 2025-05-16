7dd13ec81896a91eb8dce111459ebd4362ff0ecc
tomas <tomascostapp@gmail.com>
2025-05-15 23:56:24 -0300

    Mobile

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { r as renderers } from './chunks/internal_BsTt5pTQ.mjs';
 import { c as createExports } from './chunks/entrypoint_9VtQtp0j.mjs';
-import { manifest } from './manifest_COhBLe1r.mjs';
+import { manifest } from './manifest_D-jA3A8u.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -33,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "20b3ca9b-6603-4c14-a41e-b41c92b9fc07",
+    "middlewareSecret": "21e3d45f-d9e9-45c4-8e94-d36135cb6413",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
