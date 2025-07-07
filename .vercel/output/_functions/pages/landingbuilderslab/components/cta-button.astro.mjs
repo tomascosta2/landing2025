abadf59cd401e23adaebd1511766cecf8859b9f0
Sebas Lazo <sebalazo59@gmail.com>
2025-07-07 11:13:43 -0300

    Removed .vercel folder

--- .vercel/output/_functions/pages/landingbuilderslab/components/cta-button.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../../../chunks/cta-button_BNyojafn.mjs';
-export { r as renderers } from '../../../chunks/internal_BsTt5pTQ.mjs';
-
-const page = () => _page;
-
-export { page };
